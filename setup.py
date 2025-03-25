8e3505fdfee6cb56a4da98bf3385caeab2579fc8
Oliver M. Crook <ococrook@gmail.com>
2025-03-25 10:54:05 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except Exception as e:
 
 setup(
     name="hdx_sFDR",
-    version="0.1.2",
+    version="0.1.3",
     packages=find_packages(),
     install_requires=required_packages,
     author="Oliver Crook",
